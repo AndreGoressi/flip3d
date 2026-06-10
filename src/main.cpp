cf42e9ff06d8c521e7866bd3ef040780cab0e46d
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-10 07:16:16 +0200

    Refactor dark mode icon handling in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,6 @@
 
 #include "Flip3DWindow.h"
 
-auto IsWindowsDarkModeActive = []() -> bool {
-    HKEY hKey;
-    DWORD value = 1; 
-    DWORD size = sizeof(value);
-    
-    if (RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
-        RegQueryValueExW(hKey, L"AppsUseLightTheme", nullptr, nullptr, reinterpret_cast<LPBYTE>(&value), &size);
-        RegCloseKey(hKey);
-    }
-    return (value == 0);
-};
-
-bool isDark = IsWindowsDarkModeActive();
-
-UINT iconResourceId = isDark ? 101 : 102; 
-
-HICON hThemeIcon = (HICON)LoadImageW(
-    GetModuleHandleW(nullptr), 
-    MAKEINTRESOURCE(iconResourceId), 
-    IMAGE_ICON, 
-    0, 0, 
-    LR_DEFAULTSIZE | LR_SHARED
-);
-
-if (hThemeIcon) {
-    SendMessageW(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hThemeIcon);
-    SendMessageW(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hThemeIcon);
-}
-// ============================================================================
-
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int showCommand)
 {
     Flip3DPrototype window;
@@ -41,6 +11,36 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int showCommand)
         MessageBoxW(nullptr, L"Failed to initialize the Flip3D D3D11 prototype.", kWindowTitle, MB_OK | MB_ICONERROR);
         return 1;
     }
+
+    auto IsWindowsDarkModeActive = []() -> bool {
+        HKEY hKey;
+        DWORD value = 1; 
+        DWORD size = sizeof(value);
+        
+        if (RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+            RegQueryValueExW(hKey, L"AppsUseLightTheme", nullptr, nullptr, reinterpret_cast<LPBYTE>(&value), &size);
+            RegCloseKey(hKey);
+        }
+        return (value == 0);
+    };
+
+    bool isDark = IsWindowsDarkModeActive();
+    UINT iconResourceId = isDark ? 101 : 102; 
+
+    HICON hThemeIcon = (HICON)LoadImageW(
+        GetModuleHandleW(nullptr), 
+        MAKEINTRESOURCE(iconResourceId), 
+        IMAGE_ICON, 
+        0, 0, 
+        LR_DEFAULTSIZE | LR_SHARED
+    );
+
+    HWND hwnd = window.WindowHandle();
+    if (hwnd && hThemeIcon) {
+        SendMessageW(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hThemeIcon);
+        SendMessageW(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hThemeIcon);
+    }
+    // ============================================================================
 
     const int initialShow = (showCommand == SW_HIDE) ? SW_MAXIMIZE : showCommand;
     ShowWindow(window.WindowHandle(), initialShow);
