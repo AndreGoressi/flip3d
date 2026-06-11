1bb81e72cbb7f80a5ad1fff365a906b76b77566c
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-11 21:00:51 +0200

    Update winmain.cpp

--- src/winmain.cpp
+++ src/winmain.cpp
@@ -32,7 +32,13 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int showCommand)
     if (renderHwnd && overlayHwnd) 
     {
         SetWindowPos(renderHwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
-        SetWindowPos(overlayHwnd, renderHwnd, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+        //
+        SetWindowPos(overlayHwnd, renderHwnd, 
+            overlay.GetX(), 
+            overlay.GetY(), 
+            overlay.GetWidth(), 
+            overlay.GetHeight(), 
+            SWP_NOACTIVATE); 
     }
 
     ShowWindow(renderHwnd, showCommand == SW_HIDE ? SW_MAXIMIZE : showCommand);
