ff1d145315b75dc2adb135c899a013a9e444e9ba
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-11 06:42:58 +0200

    Update winmain.cpp

--- src/winmain.cpp
+++ src/winmain.cpp
@@ -1,8 +1,22 @@
 #include <windows.h>
 #include "ShellOverlayContext.h"
+#include "Flip3DRenderer.h"
 
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int)
 {
+    Flip3DRenderer wnd;
+    if (!wnd.Initialize(instance))
+    {
+        MessageBoxW(nullptr, L"Failed to initialize the Flip3D D3D11 prototype.", kWindowTitle, MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+    const int initialShow = (showCommand == SW_HIDE) ? SW_MAXIMIZE : showCommand;
+    ShowWindow(wnd.WindowHandle(), initialShow);
+    UpdateWindow(wnd.WindowHandle());
+    return wnd.Run();
+    //------------------------------------------------------------
+    
     HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
     if (FAILED(hr)) {
         return -1;
