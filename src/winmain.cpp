86552b86aa24a15377d819b589f28d37c0d2eac5
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-11 16:16:17 +0200

    Update winmain.cpp

--- src/winmain.cpp
+++ src/winmain.cpp
@@ -4,17 +4,16 @@
 
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int)
 {
-    Flip3DRenderer wnd;
-    if (!wnd.Initialize(instance))
+    Flip3DRenderer r_stack;
+    if (!r_stack.Initialize(instance))
     {
-        MessageBoxW(nullptr, L"Failed to initialize the Flip3D D3D11 prototype.", kWindowTitle, MB_OK | MB_ICONERROR);
+        MessageBoxW(nullptr, L"Failed to initialize the Flip3D D3D11 renderer.", kWindowTitle, MB_OK | MB_ICONERROR);
         return 1;
     }
 
     const int initialShow = (showCommand == SW_HIDE) ? SW_MAXIMIZE : showCommand;
-    ShowWindow(wnd.WindowHandle(), initialShow);
-    UpdateWindow(wnd.WindowHandle());
-    return wnd.Run();
+    ShowWindow(r_stack.RenderHandle(), initialShow);
+    UpdateWindow(r_stack.RenderHandle());
     //------------------------------------------------------------
     
     HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
@@ -38,5 +37,5 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int)
     }
 
     CoUninitialize();
-    return 0;
+    return r_stack.Run();;
 }
