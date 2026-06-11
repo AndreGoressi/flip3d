bb13585f875a62d4624fcee07c29581e852e95c0
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-11 17:28:11 +0200

    Update winmain.cpp

--- src/winmain.cpp
+++ src/winmain.cpp
@@ -24,19 +24,3 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int)
 
     return 0;
 }
-
-int WINAPI Flip3d(HINSTANCE instance, HINSTANCE, PWSTR, int showCommand)
-{
-    Flip3DRenderer r_3d;
-    if (!r_3d.Initialize(instance))
-    {
-        MessageBoxW(nullptr, L"Failed to initialize the Flip3D D3D11 renderer.", kTitle, MB_OK | MB_ICONERROR);
-        return 1;
-    }
-
-    const int initialShow = (showCommand == SW_HIDE) ? SW_MAXIMIZE : showCommand;
-    ShowWindow(r_3d.RenderHandle(), initialShow);
-    UpdateWindow(r_3d.RenderHandle());
-    return r_3d.Run();
-}
-
