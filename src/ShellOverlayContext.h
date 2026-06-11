80fa2145d4beca3c08506c1632d48dfc45c08d01
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-11 23:50:18 +0200

    Update ShellOverlayContext.h

--- src/ShellOverlayContext.h
+++ src/ShellOverlayContext.h
@@ -8,27 +8,17 @@ public:
     ~ShellOverlayContext();
 
     bool Initialize(HINSTANCE instance);
-    
     void RunMessageLoop();
     void Cleanup();
-    
-    HWND ShellHandle() const { return m_hwnd; }
-    int GetX() const { return m_x; }
-    int GetY() const { return m_y; }
-    int GetWidth() const { return m_screenW; }
-    int GetHeight() const { return m_screenH; }
-
-    UINT m_shellHookMsg = 0; 
 
 private:
-    bool ApplyAcrylic();
     static LRESULT CALLBACK OverlayWndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+    bool ApplyAcrylic();
 
-    HINSTANCE m_instance = nullptr;
-    HWND m_hwnd = nullptr;
-    
-    int m_x = 0;
-    int m_y = 0;
-    int m_screenW = 0;
-    int m_screenH = 0;
+    HINSTANCE m_instance;
+    HWND      m_hwnd;
+    UINT      m_shellHookMsg;
+
+    int m_x, m_y;        // Ursprung des Arbeitsbereichs (oben links, ohne Taskleiste)
+    int m_screenW, m_screenH;
 };
