f356bc178db5a3f61eb3c46aa1ee0c37d9e0cd45
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-11 03:31:46 +0200

    Update winmain.cpp

--- src/winmain.cpp
+++ src/winmain.cpp
@@ -1,41 +1,12 @@
 #include <windows.h>
 #include "ShellOverlayContext.h"
 
-#include <windows.h>
-
-void CloseStartMenuIfOpen()
-{
-    HWND hwnd = GetForegroundWindow();
-    if (!hwnd)
-        return;
-
-    wchar_t cls[256] = {0};
-    GetClassNameW(hwnd, cls, 256);
-
-    bool isStartMenu = wcscmp(cls, L"Windows.UI.Core.CoreWindow") == 0;
-
-    bool isSearch = wcscmp(cls, L"SearchHost") == 0;
-
-    bool isStartMenuWin10 = FindWindowW(L"DV2ControlHost", nullptr) != nullptr;
-
-    if (isStartMenu || isSearch || isStartMenuWin10)
-    {
-        //keybd_event(VK_LWIN, 0, 0, 0);
-        //keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0);
-        GetModuleHandleW(L"Shell32.dll"),
-        "Shell.ToggleDesktop";
-    }
-}
-
-
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int)
 {
     HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
     if (FAILED(hr)) {
         return -1;
     }
-
-    CloseStartMenuIfOpen();
 
     {
         ShellOverlayContext overlay;
