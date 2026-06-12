a79e7de4fea478505285b14878dd8dd11de7c1a4
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-12 18:53:33 +0200

    Implement window minimize hook and update main function

--- src/winmain.cpp
+++ src/winmain.cpp
@@ -2,7 +2,7 @@
 #include "Flip3DRenderer.h"
 #include "Config.h"
 
-int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int showCommand)
+/*int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int showCommand)
 {
     HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
     if (FAILED(hr)) return -1;
@@ -26,4 +26,67 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int showCommand)
     int result = rnd.Run();
     CoUninitialize();
     return result;
+}*/
+
+#include <dwmapi.h> 
+
+
+
+HWINEVENTHOOK g_minimizeHook = nullptr;
+
+void CALLBACK WinEventProc(
+    HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd,
+    LONG idObject, LONG idChild, DWORD dwEventThread, DWORD dwmsEventTime)
+{
+
+    if (event == EVENT_SYSTEM_MINIMIZESTART && idObject == OBJID_WINDOW)
+    {
+
+        wchar_t className[256];
+        GetClassNameW(hwnd, className, 256);
+        if (wcscmp(className, L"Flip3DWindowClass") == 0)
+            return;
+
+
+        DWORD cloakAttribute = DWM_CLOAKED_APP;
+        DwmSetWindowAttribute(hwnd, DWMWA_CLOAK, &cloakAttribute, sizeof(cloakAttribute));
+
+        ShowWindow(hwnd, SW_HIDE);
+    }
+}
+
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int showCommand)
+{
+    HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED); 
+    if (FAILED(hr)) return -1; 
+    g_minimizeHook = SetWinEventHook(
+        EVENT_SYSTEM_MINIMIZESTART, EVENT_SYSTEM_MINIMIZESTART,
+        nullptr, WinEventProc, 0, 0, WINEVENT_OUTOFCONTEXT);
+
+    Flip3DRenderer rnd; 
+    if (!rnd.Initialize(instance)) 
+    {
+        MessageBoxW(nullptr, L"Failed to initialize.", kTitle, MB_OK | MB_ICONERROR); 
+        if (g_minimizeHook) UnhookWinEvent(g_minimizeHook); 
+        CoUninitialize(); 
+        return 1; 
+    }
+
+    HWND renderHwnd = rnd.RenderHandle(); 
+
+    ShowWindow(renderHwnd, showCommand == SW_HIDE ? SW_MAXIMIZE : showCommand); 
+    UpdateWindow(renderHwnd); 
+
+    SetForegroundWindow(renderHwnd); 
+    SetActiveWindow(renderHwnd); 
+
+    int result = rnd.Run(); 
+
+    if (g_minimizeHook)
+    {
+        UnhookWinEvent(g_minimizeHook);
+    }
+
+    CoUninitialize(); 
+    return result; 
 }
