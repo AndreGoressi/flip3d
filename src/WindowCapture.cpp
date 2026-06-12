0ae318c49544750223f4e79610930146bdb19650
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-12 04:57:13 +0200

    Update WindowCapture.cpp

--- src/WindowCapture.cpp
+++ src/WindowCapture.cpp
@@ -214,7 +214,7 @@ HRESULT WindowCapture::InitViaThumbnail(HWND hwndCapture, HWND hwndDestination)
                                 | DWM_TNP_RECTDESTINATION 
                                 | DWM_TNP_ENABLE3D 
                                 | DWM_TNP_DISABLEFORCECVI
-                                | DWM_TNP_SOURCECLIENTAREAONLY;
+                                //| DWM_TNP_SOURCECLIENTAREAONLY;
     thumbProps.fVisible       = TRUE;
     thumbProps.rcDestination  = { 0, 0, srcSize.cx, srcSize.cy };
 
