f1037d6d7d44c71a307736002a22ec64e62aa533
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-13 19:34:25 +0200

    Update Capture.cpp

--- src/Capture.cpp
+++ src/Capture.cpp
@@ -222,6 +222,16 @@ BOOL CALLBACK CollectFlip3DWindowRects(HWND hwnd, LPARAM lParam)
     {
         return TRUE;
     }
+    
+    wchar_t className[256]; wchar_t windowTitle[256];
+    GetClassNameW(hWnd, className, 256); GetWindowTextW(hWnd, windowTitle, 256);
+    //
+    if (wcscmp(exeName, L"SnippingTool.exe") == 0 && wcscmp(className, L"XamlWindow") == 0) 
+    {
+        if (wcscmp(windowTitle, L"Snipping Tool") != 0 && wcscmp(windowTitle, L"") != 0) {
+            return TRUE; 
+        }
+    }
 
     DWORD cloaked = 0;
     if (SUCCEEDED(DwmGetWindowAttribute(hwnd, DWMWA_CLOAKED, &cloaked, sizeof(cloaked))) && cloaked != 0)
