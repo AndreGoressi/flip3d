f7fa96c031464c48396a1eeaa79b0ff01acb6a02
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-13 21:02:43 +0200

    Update Flip3DRenderer.cpp

--- src/Flip3DRenderer.cpp
+++ src/Flip3DRenderer.cpp
@@ -856,15 +856,13 @@ void Flip3DRenderer::SelectThumbnail(HWND targetHwnd)
 
     if (m_selectedWindowWasMinimized && m_selectedHWND)
     {
-        //ShowWindowAsync(m_selectedHWND, SW_SHOWNA);
         BOOL disable = TRUE;
         DwmSetWindowAttribute(m_selectedHWND, DWMWA_TRANSITIONS_FORCEDISABLED, &disable, sizeof(disable));
         //
         SetWindowLongPtrW(m_selectedHWND, GWL_EXSTYLE, GetWindowLongPtrW(m_selectedHWND, GWL_EXSTYLE) | WS_EX_LAYERED);
         SetLayeredWindowAttributes(m_selectedHWND, 0, 0, LWA_ALPHA);
 
-        //SetWindowPos(m_selectedHWND, HWND_TOP, 0, 0, 0, 0, 
-        //SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW | SWP_NOACTIVATE);
+        ShowWindowAsync(m_selectedHWND, SW_SHOWNOACTIVATE);
           
         m_selectedWindowActivationDispatched = true;
     }
