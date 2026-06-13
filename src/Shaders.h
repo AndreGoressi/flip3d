372b5748b0566ceed5832d0aba29a01546b62600
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-13 05:23:31 +0200

    Update Shaders.h

--- src/Shaders.h
+++ src/Shaders.h
@@ -106,9 +106,12 @@ cbuffer ObjectCB : register(b1)
 float4 main(float4 position : SV_POSITION, float2 uv : TEXCOORD0, float4 colorIn : COLOR0, float4 accentIn : COLOR1, float4 flagsIn : COLOR2) : SV_TARGET
 {
     float4 windowColor = cardTexture.Sample(cardSampler, uv);
+
+    windowColor.rgb = saturate(windowColor.rgb);  
+
     if (flagsIn.x > 0.5f)
     {
-        windowColor.rgb = windowColor.rgb / (1.0f + windowColor.rgb);
+        windowColor.rgb = windowColor.rgb * 0.6f;  
     }
 
     uint width = 0;
