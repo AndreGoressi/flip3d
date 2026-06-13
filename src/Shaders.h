036415b54735b6daf9a5dbd6341b1d674d08dbf0
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-13 04:39:01 +0200

    Update Shaders.h

--- src/Shaders.h
+++ src/Shaders.h
@@ -109,8 +109,7 @@ float4 main(float4 position : SV_POSITION, float2 uv : TEXCOORD0, float4 colorIn
 
     if (flagsIn.x > 0.5f)
     {
-        // sanfteres Tonemapping nur für minimierte Karten
-        windowColor.rgb = windowColor.rgb * 0.6f;
+        windowColor.rgb = windowColor.rgb * 1.0f;  // testweise neutral
     }
 
     uint width = 0;
