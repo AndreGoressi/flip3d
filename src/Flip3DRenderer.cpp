5d0ea59eb1b4f5d6556442710137f8eb5cb63af0
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-14 07:41:55 +0200

    Remove Sleep(0) from Flip3DRenderer
    Removed unnecessary Sleep(0) call in window rendering.

--- src/Flip3DRenderer.cpp
+++ src/Flip3DRenderer.cpp
@@ -912,13 +912,11 @@ void Flip3DRenderer::SelectThumbnail(HWND targetHwnd)
         ShowWindow(m_selectedHWND, SW_HIDE);
         ShowWindow(m_selectedHWND, SW_RESTORE);
         
-        Sleep(0);
-    
         disable = FALSE;
         DwmSetWindowAttribute(m_selectedHWND, DWMWA_TRANSITIONS_FORCEDISABLED, &disable, sizeof(disable));
   
         SetWindowPos(m_selectedHWND, HWND_TOP, 0, 0, 0, 0,
-            SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_ASYNCWINDOWPOS);
+            SWP_NOSIZE | SWP_NOACTIVATE | SWP_ASYNCWINDOWPOS);
         
         DwmFlush();
     }
