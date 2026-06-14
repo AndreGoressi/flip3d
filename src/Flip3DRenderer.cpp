28bc8f73fa6ce8615838884233fd10f74a3c1bf1
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-14 07:35:19 +0200

    Update Flip3DRenderer.cpp

--- src/Flip3DRenderer.cpp
+++ src/Flip3DRenderer.cpp
@@ -917,8 +917,8 @@ void Flip3DRenderer::SelectThumbnail(HWND targetHwnd)
         disable = FALSE;
         DwmSetWindowAttribute(m_selectedHWND, DWMWA_TRANSITIONS_FORCEDISABLED, &disable, sizeof(disable));
   
-        //SetWindowPos(m_selectedHWND, HWND_TOP, 0, 0, 0, 0, 
-             //SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW | SWP_NOACTIVATE);
+        SetWindowPos(m_selectedHWND, HWND_TOP, 0, 0, 0, 0,
+            SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_ASYNCWINDOWPOS);
         
         DwmFlush();
     }
