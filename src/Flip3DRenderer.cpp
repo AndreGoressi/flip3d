38d7f375b915264e5bd80c02379fb1efc08d8404
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-06-17 09:47:07 +0200

    Update Flip3DRenderer.cpp

--- src/Flip3DRenderer.cpp
+++ src/Flip3DRenderer.cpp
@@ -578,17 +578,10 @@ void Flip3DRenderer::ApplyAeroPeek(BOOL enable)
     }
 
     if (!pDwmpActivateLivePreview) return;
-    //
+
     if (aeroPeekActive != enable)
     {
-        if (enable)
-        {
-            pDwmpActivateLivePreview(TRUE, nullptr, nullptr, 3, (LPVOID)0x3244);
-        }
-        else
-        {
-            pDwmpActivateLivePreview(FALSE, nullptr, nullptr, 3, nullptr);
-        }
+        pDwmpActivateLivePreview(enable, nullptr, nullptr, 3, nullptr);
         aeroPeekActive = enable;
     }
 }
